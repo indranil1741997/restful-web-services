cf09d0fe80bf02f7763375943c9a3e25a8e180b1
indranil1741997 <indranil1741997@gmail.com>
2020-11-07 20:36:34 +0530

    Adding i18n

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,14 +1,24 @@
 package com.in28minutes.rest.webservices.restfulwebservices.controller;
 
 import com.in28minutes.rest.webservices.restfulwebservices.model.HelloWorldBean;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 @Component
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello-world")
     public String helloWorld()
@@ -26,5 +36,10 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name)
     {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping("/hello-world/internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 }
