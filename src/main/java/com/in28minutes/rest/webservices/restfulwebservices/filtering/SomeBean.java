3b02b1592dd0a5a0b8ec234a9f46e288779a4501
indranil1741997 <indranil1741997@gmail.com>
2020-11-07 23:26:28 +0530

    Adding Dynamic Filtering

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -0,0 +1,15 @@
+package com.in28minutes.rest.webservices.restfulwebservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@JsonFilter("SomeBeanFilter")
+public class SomeBean {
+
+    private String field1;
+    private String field2;
+    private String field3;
+}
