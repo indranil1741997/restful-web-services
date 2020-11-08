addaee1dac8d8b228184226a0d248407603d4813
indranil1741997 <indranil1741997@gmail.com>
2020-11-08 13:53:57 +0530

    Adding JPA

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/model/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/model/User.java
@@ -2,14 +2,26 @@ package com.in28minutes.rest.webservices.restfulwebservices.model;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @ApiModel(description = "All details about the user")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least two characters")
@@ -19,36 +31,6 @@ public class User {
     @Past
     @ApiModelProperty(notes = "Birth date should be in the past")
     private Date birthDate;
-
-    public User(Integer id, String name, Date birthDate) {
-        this.id = id;
-        this.name = name;
-        this.birthDate = birthDate;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
 
     @Override
     public String toString() {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'Indranil1');
+insert into user values(2, sysdate(), 'Indranil2');
+insert into user values(3, sysdate(), 'Indranil3');
